83a544183d384462c943295395400ed8c3b10bb6
jonathan <jr2014@ic.ac.uk>
2018-02-11 12:36:55 +0000

    actually made it work...

--- pre_process_npy.py
+++ pre_process_npy.py
@@ -1,15 +1,18 @@
 import numpy as np
+import os
 
 train_testsplit = 0.6 ##
 
 input_folder = "specdata"
 
+filename_list = []
+
 for filez in os.listdir(input_folder):
     if filez.endswith(".npy"):
-        filename_list.append(os.path.join(input_folder, file))
+        filename_list.append(os.path.join(input_folder, filez))
 
 for filename in filename_list:
     x = np.load(filename)
     x_len = len(x)
-    numpy.random.shuffle(x)
+    np.random.shuffle(x)
     training_tmp, test_tmp = x[:int(x_len*train_testsplit),:], x[int(x_len*train_testsplit):,:]
