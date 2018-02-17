bde5fe05222ef6a70f182ce918e25b9f087f8c43
jonathan <jr2014@ic.ac.uk>
2018-02-17 22:58:41 +0000

    Added real time classification

--- zmqclient.py
+++ zmqclient.py
@@ -6,6 +6,27 @@ port = "5555"
 
 import signaldoctorlib as sdl
 
+MODEL_NAME = "spec_model"
+
+from keras.models import model_from_json
+
+
+def classify_spectrogram(input_array, model):
+    
+    print("Classify Spec")
+    print("Data Shape:", input_array.shape)
+    
+    print(input_array)
+    input_array = input_array.reshape((1, input_array.shape[0], input_array.shape[1], 1))
+    
+    prediction = loaded_model.predict(input_array)
+    
+    print(prediction)
+    
+    
+    prediction = prediction.flat[0]
+    
+
 
 if __name__ == "__main__":
     
@@ -17,9 +38,30 @@ if __name__ == "__main__":
 
     socket.setsockopt_string(zmq.SUBSCRIBE, "")
     
+    
+    ## LOAD SPECTROGRAM NETWORK ##
+    json_file = open('%s.nn'%(MODEL_NAME), 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("%s.h5"%(MODEL_NAME))
+    print("Loaded model from disk")
+    
     while True:
         string = socket.recv()
         buffer_data = np.fromstring(string, dtype = 'complex64')
-        sdl.process_buffer(buffer_data)
+        extracted_features = sdl.process_buffer(buffer_data)
+        
+        
+        print(type(loaded_model))
+        
+        spec = np.asarray(extracted_features[0][0])
+        
+        classify_spectrogram(spec, loaded_model)
+        
         sys.exit(1)
+    
+
+
     
