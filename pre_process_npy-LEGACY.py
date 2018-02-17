64f2c58a0f76f9a57bac62d40b2a7b5efbf0adb2
jonathan <jr2014@ic.ac.uk>
2018-02-17 22:57:56 +0000

    Broken file - will be removed soon

--- pre_process_npy-LEGACY.py
+++ pre_process_npy-LEGACY.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 
 ## Takes training IQ data and generates training features
 
+
+data_output_folder_spec = "specdata"
+data_output_folder_psd = "psddata"
+
+data_lib = [data_output_folder_spec, data_output_folder_psd]
+
 def load_npz(filename):
     data = np.load(filename)
     iq_data = data['channel_iq']
@@ -12,17 +18,19 @@ def load_npz(filename):
     return fs, iq_data
 
 def feature_gen(file_list, spec_size):
-    output_list = [] ## This is going to be very big!
+    output_list_spec = [] ## This is going to be very big!
+    output_list_psd = []
     for filename in file_list:
         fs, iq_data = load_npz(filename)
         print("%i samples loaded at a rate of %f" % (len(iq_data), fs))
         print("We have %f s of data" % (len(iq_data)/fs))
 
-        Zxx_dat = sdl.generate_features(fs, iq_data, 1024, False)
-        output_list.append(Zxx_dat)
+        Zxx_dat, PSD = sdl.generate_features(fs, iq_data, spec_size, False)
+        output_list_spec.append(Zxx_dat)
+        output_list_psd.append(PSD)
         #plt.pcolormesh(Zxx_dat)
         #plt.show()
-    return output_list
+    return output_list_spec, output_list_psd
 
 
 input_folder = "/mnt/datastore/FYP/training_sets/TF_Train_V3/iq"
@@ -37,17 +45,22 @@ for sig in os.listdir(input_folder):
             if fileZ.endswith(".npz"):
                 filename_list.append(os.path.join(sig_input_folder, fileZ))
     
-        spec_list = feature_gen(filename_list, 256)
+        spec_list, psd_list = feature_gen(filename_list, 256)
+        
         spec_aray = np.asarray(spec_list)
-        np.save(sig + '.npy', spec_aray)
-
+        psd_aray = np.asarray(psd_list)
+        
+        np.save(data_output_folder_spec + "/" + sig + '.npy', spec_aray)
+        np.save(data_output_folder_psd + "/" + sig + '.npy', psd_aray)
 
 
 ## Splits training data into training and test data
 
-train_testsplit = 0.6 ##
+##### SPECTROGRAM ########
 
-input_folder = "specdata"
+input_folder = data_output_folder_spec
+
+train_testsplit = 0.6 ##
 
 filename_list = []
 y_train = []
@@ -77,7 +90,9 @@ for filename in filename_list:
 
 y_train = np.asarray(y_train)
 
+
 X_train = np.asarray(X_train)
+print(X_train.shape)
 X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1))
 
 y_test = np.asarray(y_test)
@@ -85,4 +100,51 @@ y_test = np.asarray(y_test)
 X_test = np.asarray(X_test)
 X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], 1))
     
-np.savez("TrainingData.npz", X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+np.savez("%sLib.npz" % (input_folder), X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
+
+
+##### PSD ########
+
+input_folder = data_output_folder_psd
+
+train_testsplit = 0.6 ##
+
+filename_list = []
+y_train = []
+X_train = []
+y_test = []
+X_test = []
+
+for filez in os.listdir(input_folder):
+    if filez.endswith(".npy"):
+        filename_list.append(os.path.join(input_folder, filez))
+
+index = 0
+for filename in filename_list:
+    x = np.load(filename)
+    x_len = len(x)
+    np.random.shuffle(x)
+    training_tmp, test_tmp = x[:int(x_len*train_testsplit),:], x[int(x_len*train_testsplit):,:]
+    for i in training_tmp:
+        X_train.append(i)
+        y_train.append(index)
+    
+    for i in test_tmp:
+        X_test.append(i)
+        y_test.append(index)
+    
+    index = index + 1
+
+y_train = np.asarray(y_train)
+
+
+X_train = np.asarray(X_train)
+print(X_train.shape)
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1]))
+
+y_test = np.asarray(y_test)
+
+X_test = np.asarray(X_test)
+X_test = X_test.reshape((X_test.shape[0], X_test.shape[1]))
+    
+np.savez("%sLib.npz" % (input_folder), X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
