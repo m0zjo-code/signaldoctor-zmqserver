6cb383f6a1884d8a7d7e132457df196629b181fe
jonathan <jr2014@ic.ac.uk>
2018-04-11 15:30:36 +0100

    Added model choice

--- CNN_MagSpec.py
+++ CNN_MagSpec.py
@@ -2,6 +2,9 @@ import os
 
 USE_GPU = False
 
+USE_PRE_TRAINED_NETWORK = False
+
+
 ## Set up Tensorflow to use CPU or GPU
 # Disable Tensorflow GPU computation (seems to break when loading inception)
 if not USE_GPU:
@@ -24,7 +27,7 @@ from keras.callbacks import TensorBoard
 from time import time
 
 batch_size = 64 # in each iteration, we consider 32 training examples at once
-num_epochs = 2 # we iterate 20 times over the entire training set
+num_epochs = 2000 # we iterate 2000 times over the entire training set
 kernel_size = 3 # we will use 3x3 kernels throughout
 pool_size = 2 # we will use 2x2 pooling throughout
 conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
@@ -58,62 +61,61 @@ X_test /= np.max(X_test) # Normalise data to [0, 1] range
 Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
 Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
 
+if not USE_PRE_TRAINED_NETWORK:
+    ### Set up CNN model ##
+    inp = Input(shape=(height, width, depth)) # depth goes last in TensorFlow back-end (first in Theano)
+    # Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
+    conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
+    conv_2 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(conv_1)
+    pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+    drop_1 = Dropout(drop_prob_1)(pool_1)
+    # Conv [64] -> Conv [64] -> Pool (with dropout on the pooling layer)
+    conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
+    conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(conv_3)
+    pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
+    drop_2 = Dropout(drop_prob_1)(pool_2)
+    # Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
+    flat = Flatten()(drop_2)
+    hidden = Dense(hidden_size_2, activation='relu')(flat)
+    drop_3 = Dropout(drop_prob_2)(hidden)
+    out = Dense(num_classes, activation='softmax')(drop_3)
 
-### Set up model ##
-inp = Input(shape=(height, width, depth)) # depth goes last in TensorFlow back-end (first in Theano)
-# Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
-conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(inp)
-conv_2 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(conv_1)
-pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
-drop_1 = Dropout(drop_prob_1)(pool_1)
-# Conv [64] -> Conv [64] -> Pool (with dropout on the pooling layer)
-conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
-conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(conv_3)
-pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
-drop_2 = Dropout(drop_prob_1)(pool_2)
-# Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
-flat = Flatten()(drop_2)
-hidden = Dense(hidden_size_2, activation='relu')(flat)
-drop_3 = Dropout(drop_prob_2)(hidden)
-out = Dense(num_classes, activation='softmax')(drop_3)
+    model_final = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
 
-model_final = Model(inputs=inp, outputs=out) # To define a model, just specify its input and output layers
+    #########################
 
-#########################
+elif USE_PRE_TRAINED_NETWORK:
+    from keras import applications
+    X_train_tmp = np.zeros((X_train.shape[0], X_train.shape[1], X_train.shape[2], 3))
+    for i in range(0, len(X_train)):
+        for j in range(0, 3):
+            X_train_tmp[i,:,:,j] = X_train[i, :, :, 0]
+    X_train = X_train_tmp
 
+    X_test_tmp = np.zeros((X_test.shape[0], X_test.shape[1], X_test.shape[2], 3))
+    for i in range(0, len(X_test)):
+        for j in range(0, 3):
+            X_test_tmp[i,:,:,j] = X_test[i, :, :, 0]
+    X_test = X_test_tmp
 
+    print(X_test.shape)
 
-####from keras import applications
-####X_train_tmp = np.zeros((X_train.shape[0], X_train.shape[1], X_train.shape[2], 3))
-####for i in range(0, len(X_train)):
-    ####for j in range(0, 3):
-        ####X_train_tmp[i,:,:,j] = X_train[i, :, :, 0]
-####X_train = X_train_tmp
+    model = applications.inception_v3.InceptionV3(include_top=True, weights='imagenet', input_shape = (height, width, 3), classes=num_classes)
 
-####X_test_tmp = np.zeros((X_test.shape[0], X_test.shape[1], X_test.shape[2], 3))
-####for i in range(0, len(X_test)):
-    ####for j in range(0, 3):
-        ####X_test_tmp[i,:,:,j] = X_test[i, :, :, 0]
-####X_test = X_test_tmp
+    #model = applications.mobilenet.MobileNet(include_top=False, weights='imagenet', input_shape = (height, width, 3), classes=num_classes)
 
-####print(X_test.shape)
+    for layer in model.layers[:19]:
+        layer.trainable = False
 
-####model = applications.inception_v3.InceptionV3(include_top=False, weights='imagenet', input_shape = (height, width, 3), classes=num_classes)
+    x = model.output
+    x = Flatten()(x)
+    x = Dropout(drop_prob_1)(x)
+    x = Dense(hidden_size_1, activation="relu")(x)
+    x = Dropout(drop_prob_2)(x)
+    x = Dense(hidden_size_2, activation="relu")(x)
+    predictions = Dense(num_classes, activation="softmax")(x)
 
-#####model = applications.mobilenet.MobileNet(include_top=False, weights='imagenet', input_shape = (height, width, 3), classes=num_classes)
-
-####for layer in model.layers[:19]:
-    ####layer.trainable = False
-
-####x = model.output
-####x = Flatten()(x)
-####x = Dropout(drop_prob_1)(x)
-####x = Dense(hidden_size_1, activation="relu")(x)
-####x = Dropout(drop_prob_2)(x)
-####x = Dense(hidden_size_2, activation="relu")(x)
-####predictions = Dense(num_classes, activation="softmax")(x)
-
-####model_final = Model(input = model.input, output = predictions)
+    model_final = Model(input = model.input, output = predictions)
 
 
 
