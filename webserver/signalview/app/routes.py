cfd4b082b15b925011d9c76e5325933258e2a0ed
jonathan <jr2014@ic.ac.uk>
2018-04-25 23:53:30 +0100

    Changed to plotly

--- webserver/signalview/app/routes.py
+++ webserver/signalview/app/routes.py
@@ -31,21 +31,40 @@ def graph():
     cur.execute("SELECT * from signal_store.signal_live order by timestamp desc limit %i" % no_records)
     sig_data = cur.fetchall()
     cur.execute("SELECT * from signal_store.signal_live_global")
-    psd = cur.fetchall()[0]
+    g_dat = cur.fetchall()
     
-    return render_template('graph.html', title='SignalView', user=user, sig_data=sig_data, psd=extract_psd(psd, sig_data))
+    return render_template('graph.html', title='SignalView', user=user, sig_data=sig_data, psd=extract_psd(g_dat, sig_data), labels=generate_labels(g_dat, sig_data))
 
-def extract_psd(psd, sig_data):
-    dat = psd[0]
+def extract_psd(g_dat, sig_data):
+    print(len(g_dat))
+    dat = g_dat[0][0]
     dat = dat.replace('{','')
     dat = dat.replace('}','')
     dat_list = dat.split(',')
+    
+    cf = g_dat[0][1]
+    fs = g_dat[0][2]
+    buf_len = g_dat[0][3]
+    rsr = g_dat[0][4]
     
     print(os.listdir())
     
     with open('app/static/psd.csv', 'w') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        
+        spamwriter.writerow(['Frequency', 'psd'])
         for i in range(0, len(dat_list)):
-            spamwriter.writerow([i, dat_list[i]])
+            spamwriter.writerow([(fs * (i*rsr - buf_len/2)/buf_len) + cf, dat_list[i]])
+    
+def generate_labels(g_dat, sig_data):
+    outx = "["
+    outy = "["
+    out_l = "["
+    for i in sig_data:
+        outx = outx + i[7] + ","
+        outy = outy + "1,"
+        out_l = out_l + '3,'
+    outx = outx[:-1] + "]"
+    outy = outy[:-1] + "]"
+    out_l = out_l[:-1] + "]"
+    return [outx, outy, out_l]
     
