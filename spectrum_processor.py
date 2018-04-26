5c3d5259dd14a4603b6256d5f4886b511f7ed12c
jonathan <jr2014@ic.ac.uk>
2018-04-26 14:59:37 +0100

    Added proper argument processing

--- spectrum_processor.py
+++ spectrum_processor.py
@@ -7,6 +7,7 @@ pubport = 5556
 pubport_global = 5558
 
 import signaldoctorlib as sdl
+import argparse
 
 LOG_IQ = True
 #MODEL_NAME = "specmodel"
@@ -18,7 +19,8 @@ fs = 2.09715e6
 
 metadata = {'radio':'HackRF', 'cf':3.9e6}
 
-def main(argv):
+
+def main(args):
     IQ_LOCAL = None
     IQ_FILE = None
     
@@ -32,46 +34,52 @@ def main(argv):
     pubsocket_global = pubcontext_global.socket(zmq.PUB)
     pubsocket_global.bind('tcp://127.0.0.1:%i' % pubport_global)
     
-    try:
-        opts, args = getopt.getopt(argv,"hli:",["ifile="])
-    except getopt.GetoptError:
-        print('%s -i <inputfilepath>'%sys.argv[0])
-        sys.exit(2)
-    
-    for opt, arg in opts:
-        if opt == '-h':
-            print('%s -i <inputfilepath>'%sys.argv[0])
-            sys.exit()
-        if opt == '-l':
-            IQ_LOCAL = False
-        elif opt in ("-i", "--ifile"):
-            IQ_LOCAL = True
-            IQ_FILE = arg
-    
-    if (len(opts) == 0):
-        print("Using default parameters ->> Loading data from ZMQ PUB")
-    
-    if  IQ_LOCAL:
+    if args.input_file != None:
         print("Reading local IQ")
-        print("Input file: ", IQ_FILE)
-        sdl.process_iq_file(IQ_FILE,LOG_IQ, pubsocket=[pubsocket, pubsocket_global], metadata=metadata)
+        print("Input file: ", args.input_file)
+        sdl.process_iq_file(args.input_file, LOG_IQ, pubsocket=[pubsocket, pubsocket_global], metadata=metadata)
         sys.exit()
+    
+    if args.d:
+        # Socket to talk to server
+        context = zmq.Context()
+        socket = context.socket(zmq.SUB)
+        print("Collecting updates from IQ server...")
+        socket.connect ("tcp://127.0.0.1:%i" % port)
 
-    # Socket to talk to server
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    print("Collecting updates from IQ server...")
-    socket.connect ("tcp://127.0.0.1:%i" % port)
+        socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
-    socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        #loaded_model, index_dict = sdl.get_spec_model(MODEL_NAME)
 
-    #loaded_model, index_dict = sdl.get_spec_model(MODEL_NAME)
+        while True:
+            string = socket.recv()
+            buffer_data = np.fromstring(string, dtype = 'complex64')
+            sdl.classify_buffer(buffer_data, fs=fs, LOG_IQ=LOG_IQ, pubsocket=[pubsocket, pubsocket_global], metadata=metadata)
+    
+    if args.zmq_src != None:
+        # Socket to talk to server
+        context = zmq.Context()
+        socket = context.socket(zmq.SUB)
+        print("Collecting updates from IQ server...")
+        socket.connect ("tcp://%s" % args.zmq_src)
 
-    while True:
-        string = socket.recv()
-        buffer_data = np.fromstring(string, dtype = 'complex64')
-        sdl.classify_buffer(buffer_data, fs=fs, LOG_IQ=LOG_IQ, pubsocket=pubsocket, metadata=metadata)
+        socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
+        #loaded_model, index_dict = sdl.get_spec_model(MODEL_NAME)
+
+        while True:
+            string = socket.recv()
+            buffer_data = np.fromstring(string, dtype = 'complex64')
+            sdl.classify_buffer(buffer_data, fs=fs, LOG_IQ=LOG_IQ, pubsocket=[pubsocket, pubsocket_global], metadata=metadata)
+    
+    
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(description='### Radio Interface for Signal Doctor. Both GRC and *.WAV Inputs Supported - Jonathan Rawlinson 2018 ###', epilog="For more help please see the docs")
+
+    parser.add_argument('-i', help='Process WAV file (single or dual channel)', action="store", dest="input_file")
+    parser.add_argument('-z', help='RX from GRC -->> source_ip:port', action="store", dest="zmq_src")
+    parser.add_argument('-d', help='RX from GRX with default settings (127.0.0.1:5555)', action="store_true")
+    
+    args = parser.parse_args()
+    main(args)
