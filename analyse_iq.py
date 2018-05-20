8f883dc3e21ed98e97d37f3377b712bf5cad4392
jonathan <jr2014@ic.ac.uk>
2018-05-20 14:02:21 +0100

    Report generation logs added and analyse IQ setup to generate graohs

--- analyse_iq.py
+++ analyse_iq.py
@@ -1,30 +1,45 @@
 import os
 import numpy as np
-import math
+import math, configparser
 import matplotlib.pyplot as plt
 import signaldoctorlib as sdl
 
-load_dir = 'logs/'
+load_dir = 'MATLAB/sample_logs/'
 
-items = os.listdir(load_dir)
+iq_file = '0SA1QT'
 
-iqlist = []
-for names in items:
-    if names.endswith(".npz"):
-        iqlist.append(names)
-print(iqlist)
+config = configparser.ConfigParser()
+config.read('sdl_config.ini')
 
-def savefig_mat(mat, fname):
-    plt.pcolormesh(mat)
-    #plt.show()
-    plt.savefig(fname+".jpg", dpi=100, bbox_inches='tight')
+data = np.load(load_dir+iq_file+'.npz')
+#print(data.keys())
+channel_iq = data['channel_iq']
+fs = data['fs']
+iq_len = len(channel_iq)
+print("IQ Len: ", iq_len, iq_len/fs, "s")
+feature_list = sdl.generate_features(fs,  channel_iq, config=config)
 
-for iq_file in iqlist:
-    data = np.load(load_dir+iq_file)
-    #print(data.keys())
-    channel_iq = data['channel_iq']
-    fs = data['fs']
-    iq_len = len(channel_iq)
-    print("IQ Len: ", iq_len, iq_len/fs, "s")
-    feature_list = sdl.generate_features(fs,  channel_iq)
-    savefig_mat(feature_list[0], load_dir+iq_file)
+##### FYI::
+    ###output_dict = {}
+    ###output_dict['magnitude'] = Zxx_mag_rs
+    ###output_dict['phase'] = Zxx_phi_rs
+    ###output_dict['corrcoef'] = Zxx_cec_rs
+    ###output_dict['cov'] = Zxx_cov_rs
+    ###output_dict['psd'] = PSD
+    ###output_dict['variencespectrum'] = Varience_Spectrum
+    ###output_dict['differentialspectrumdensity'] = Differential_Spectrum
+    ###output_dict['differentialspectrum_freq'] = diff_array0_rs
+    ###output_dict['differentialspectrum_time'] = diff_array1_rs
+    ###output_dict['min_spectrum'] = Min_Spectrum
+    ###output_dict['max_spectrum'] = Max_Spectrum
+    ###output_dict['fft_spectrum'] = normalise_spectrogram(Zxx_mag_fft, spec_size, spec_size)
+    ###output_dict['hilb_spectrum'] = normalise_spectrogram(Zxx_mag_hilb, spec_size, spec_size)
+#####
+
+plt.title('Magnitude Spectrogram - CW')
+plt.pcolormesh(np.linspace(0, iq_len/fs, 256), np.linspace(-fs/2, fs/2, 256), feature_list['magnitude'])
+plt.xlabel('Time/s')
+plt.ylabel('Frequency/Hz')
+plt.show()
+
+
