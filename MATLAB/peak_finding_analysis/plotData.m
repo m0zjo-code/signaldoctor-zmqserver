7fed1a04b5183efc067b2af516b081c536aa16e3
unknown <jr2014@eews506a-030.ic.ac.uk>
2018-05-22 12:07:17 +0100

    log

--- MATLAB/peak_finding_analysis/plotData.m
+++ MATLAB/peak_finding_analysis/plotData.m
@@ -5,7 +5,7 @@ A = table2array(T(:,2:4));
 %% Plot data
 
 figure(1)
-hBar = bar(sqrt(A(:,2)));
+hBar = bar(log(1000000*A(:,2)));
 xt = get(gca, 'XTick');
 set(gca, 'XTick', xt, 'XTickLabel', {
     'scipy-cwt'
@@ -22,6 +22,6 @@ xtickangle(45)
 grid on
 grid minor
 
-ylabel('sqrt(Time(s))')
+ylabel('log(Calculation Time(s))')
 xlabel('Peak Detection Algorithms','FontSize',12)
 title('Peak Detection Computation Time Results')
