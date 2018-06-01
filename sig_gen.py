2c1394765f6a94bb5a04337a820ba8c24ea3c1eb
jonathan <jr2014@ic.ac.uk>
2018-06-01 16:27:47 +0100

    Removed rolling issue

--- sig_gen.py
+++ sig_gen.py
@@ -20,19 +20,25 @@ for i in range(0, no_sigs):
     noise_vec.imag = np.random.normal(0, 1, 8192)
 
     spec_size = 256
-    feature_dict = sdl.generate_features(1, noise_vec, spec_size, plot_features = False, config = config, roll=False)
+    feature_dict = sdl.generate_features(1, noise_vec, spec_size, plot_features = False, config = config)
     feature_dict['iq_data'] = noise_vec
     feature_dict['local_fs'] = 1
     sdl.save_IQ_buffer(feature_dict, output_folder = output_folder_noise,  config = config)
+    print("Generated NOISE")
 
 
 for i in range(0, no_sigs):
     noise_vec = np.empty((8192), dtype = np.complex)
     noise_vec.real = np.random.normal(1, 1, 8192)
     noise_vec.imag = np.random.normal(1, 1, 8192)
+    
+    buffer_fft = sdl.fft_wrap(noise_vec, mode = 'scipy')
+    buffer_fft_rolled = np.roll(buffer_fft, int(len(buffer_fft)/2))
+    noise_vec = sdl.ifft_wrap(buffer_fft_rolled, mode = 'scipy')
 
     spec_size = 256
-    feature_dict = sdl.generate_features(1, noise_vec, spec_size, plot_features = False, config = config, roll = False)
+    feature_dict = sdl.generate_features(1, noise_vec, spec_size, plot_features = False, config = config)
     feature_dict['iq_data'] = noise_vec
     feature_dict['local_fs'] = 1
-    sdl.save_IQ_buffer(feature_dict, output_folder = output_folder_carrier,  config = config, roll=False)
+    sdl.save_IQ_buffer(feature_dict, output_folder = output_folder_carrier,  config = config)
+    print("Generated CARRIER")
